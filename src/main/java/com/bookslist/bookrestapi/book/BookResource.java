c73a4941041c05674984a68e86926c505293419e
Anbarasan Samandan <anbarasan.samandan@gmail.com>
2020-05-29 10:55:22 +0530

    Changes to BooksResource to allow multiple origins and changes in package.json

--- src/main/java/com/bookslist/bookrestapi/book/BookResource.java
+++ src/main/java/com/bookslist/bookrestapi/book/BookResource.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins="http://localhost:3000")
+@CrossOrigin(origins= {"http://localhost:3000", "http://127.0.0.1:8081"})
 public class BookResource {
 
 	@Autowired
